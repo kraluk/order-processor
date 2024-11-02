d56a0c76371fe386694d786114add2810546fe40
Łukasz Krauzowicz <kraluk@gmail.com>
2024-11-02 11:04:53 +0100

    chore: project init

---
+++ src/test/java/io/kraluk/batchprocessor/BatchprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kraluk.batchprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
