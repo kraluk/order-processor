e346b003f31919889ae9b84a701d804a5cc2d5f8
Łukasz Krauzowicz <kraluk@gmail.com>
2024-11-03 15:39:46 +0100

    chore(config): add initial async configuration

---
+++ src/main/java/io/kraluk/batchprocessor/application/AsyncConfiguration.java
@@ -0,0 +1,22 @@
+package io.kraluk.batchprocessor.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.task.SimpleAsyncTaskExecutorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+@EnableScheduling
+class AsyncConfiguration {
+  private static final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
+
+  @Bean
+  SimpleAsyncTaskExecutor taskExecutor(final SimpleAsyncTaskExecutorBuilder builder) {
+    return builder.threadNamePrefix("async-task-").build();
+  }
+}
