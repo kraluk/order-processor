44dd685e1aa69ee266cdf87b31d248ae13278448
Łukasz Krauzowicz <kraluk@gmail.com>
2024-11-03 15:27:52 +0100

    chore(refactor): small improvements in streamops

--- src/main/java/io/kraluk/batchprocessor/shared/StreamOps.java
+++ src/main/java/io/kraluk/batchprocessor/shared/StreamOps.java
@@ -7,29 +7,42 @@ import java.util.stream.StreamSupport;
 public class StreamOps {
 
   private StreamOps() {
-    throw new UnsupportedOperationException("This class should not be instantiated");
+    throw new UnsupportedOperationException("This class should not be instantiated!");
   }
 
-  public static <T> Stream<List<T>> fixedWindow(Stream<T> stream, int size) {
-    final Iterator<T> iterator = stream.iterator();
-    final Iterator<List<T>> listIterator =
-        new Iterator<>() {
+  public static <T> Stream<List<T>> fixedWindow(Stream<T> stream, int windowSize) {
+    final Iterator<T> streamIterator = stream.iterator();
+    final Iterator<List<T>> windowIterator =
+        new FixedWindowStreamIterator<>(streamIterator, windowSize);
 
-          public boolean hasNext() {
-            return iterator.hasNext();
-          }
-
-          public List<T> next() {
-            final List<T> result = new ArrayList<>(size);
-
-            for (int i = 0; i < size && iterator.hasNext(); i++) {
-              result.add(iterator.next());
-            }
-
-            return result;
-          }
-        };
     return StreamSupport.stream(
-        Spliterators.spliteratorUnknownSize(listIterator, Spliterator.ORDERED), false);
+        Spliterators.spliteratorUnknownSize(windowIterator, Spliterator.ORDERED), false);
+  }
+
+  private static class FixedWindowStreamIterator<T> implements Iterator<List<T>> {
+
+    private final Iterator<T> streamIterator;
+    private final int windowSize;
+
+    public FixedWindowStreamIterator(Iterator<T> streamIterator, int windowSize) {
+      this.streamIterator = streamIterator;
+      this.windowSize = windowSize;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return streamIterator.hasNext();
+    }
+
+    @Override
+    public List<T> next() {
+      final List<T> result = new ArrayList<>(windowSize);
+
+      for (int i = 0; i < windowSize && streamIterator.hasNext(); i++) {
+        result.add(streamIterator.next());
+      }
+
+      return result;
+    }
   }
 }
