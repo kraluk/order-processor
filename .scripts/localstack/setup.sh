c08ce0a0c543410fee5d895223df2d8e90446727
Łukasz Krauzowicz <kraluk@gmail.com>
2024-11-09 15:58:56 +0100

    chore(config): change hikarcicp configuration and add some more test data

--- .scripts/localstack/setup.sh
+++ .scripts/localstack/setup.sh
@@ -4,3 +4,4 @@ awslocal s3 mb s3://order-updates-local
 awslocal sqs create-queue --queue-name order-updates-local
 
 awslocal s3 cp '/etc/localstack/init/ready.d/orders.csv' 's3://order-updates-local/orders.csv'
+awslocal s3 cp '/etc/localstack/init/ready.d/orders2.csv' 's3://order-updates-local/orders2.csv'
