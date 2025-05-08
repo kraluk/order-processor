d16c56b14ea3ff4760899eb22ba6ec0325678094
Łukasz Krauzowicz <kraluk@gmail.com>
2025-05-08 22:14:36 +0200

    chore(deps): bump up spring boot to 3.4.5 and gradle to 8.14

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,23 @@ rootProject.name = "order-processor"
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      plugin("spring.boot", "org.springframework.boot").version("3.4.4")
+      plugin("spring.boot", "org.springframework.boot").version("3.4.5")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
-      plugin("jooq", "nu.studer.jooq").version("10.0")
+      plugin("jooq", "nu.studer.jooq").version("10.1")
 
       plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
-      plugin("spotless", "com.diffplug.spotless").version("7.0.2")
-      plugin("errorprone", "net.ltgt.errorprone").version("4.1.0")
+      plugin("spotless", "com.diffplug.spotless").version("7.0.3")
+      plugin("errorprone", "net.ltgt.errorprone").version("4.2.0")
 
       version("springCloudAws", "3.3.0")
-      version("springDoc", "2.8.6")
+      version("springDoc", "2.8.8")
       version("transactionoutbox", "6.0.585") // BE AWARE: check migration scripts before bumping the version up
       version("javaMoney", "1.4.5")
     }
     create("testLibs") {
     }
     create("toolLibs") {
-      version("errorprone", "2.37.0")
+      version("errorprone", "2.38.0")
     }
   }
 }
