6d51e34174a289df1e0939f0bb50322919645d2e
Łukasz Krauzowicz <kraluk@gmail.com>
2025-11-21 22:20:46 +0100

    chore(deps): bump up spring boot to 3.5.8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
       version("java", "25")
     }
     create("libs") {
-      plugin("spring.boot", "org.springframework.boot").version("3.5.7")
+      plugin("spring.boot", "org.springframework.boot").version("3.5.8")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
       plugin("jooq", "nu.studer.jooq").version("10.1.1")
 
@@ -14,9 +14,9 @@ dependencyResolutionManagement {
       plugin("spotless", "com.diffplug.spotless").version("8.0.0")
       plugin("errorprone", "net.ltgt.errorprone").version("4.3.0")
 
-      version("springCloudAws", "3.4.0")
-      version("springDoc", "2.8.13")
-      version("awsSdk", "2.36.2")
+      version("springCloudAws", "3.4.1")
+      version("springDoc", "2.8.14")
+      version("awsSdk", "2.39.1")
       version("transactionoutbox", "6.1.653") // BE AWARE: check migration scripts before bumping the version up
       version("javaMoney", "1.4.5")
     }
@@ -25,8 +25,8 @@ dependencyResolutionManagement {
     }
     create("toolLibs") {
       version("jspecify", "1.0.0")
-      version("errorprone", "2.43.0")
-      version("nullaway", "0.12.10")
+      version("errorprone", "2.44.0")
+      version("nullaway", "0.12.12")
     }
   }
 }
