e264364e2e92c203f166f59b669a318269a09871
Łukasz Krauzowicz <kraluk@gmail.com>
2025-12-19 23:05:49 +0100

    chore(deps): bump up spring boot to 3.5.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
       version("java", "25")
     }
     create("libs") {
-      plugin("spring.boot", "org.springframework.boot").version("3.5.8")
+      plugin("spring.boot", "org.springframework.boot").version("3.5.9")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
       plugin("jooq", "nu.studer.jooq").version("10.1.1")
 
@@ -16,17 +16,17 @@ dependencyResolutionManagement {
 
       version("springCloudAws", "3.4.2")
       version("springDoc", "2.8.14")
-      version("awsSdk", "2.40.3")
+      version("awsSdk", "2.40.12")
       version("transactionoutbox", "6.1.653") // BE AWARE: check migration scripts before bumping the version up
       version("javaMoney", "1.4.5")
     }
     create("testLibs") {
-      version("testcontainers", "2.0.2")
+      version("testcontainers", "2.0.3")
     }
     create("toolLibs") {
       version("jspecify", "1.0.0")
-      version("errorprone", "2.44.0")
-      version("nullaway", "0.12.12")
+      version("errorprone", "2.45.0")
+      version("nullaway", "0.12.15")
     }
   }
 }
