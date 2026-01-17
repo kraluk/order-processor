bacac366d890a4c372e8f4c52a576a2ce481e627
Łukasz Krauzowicz <kraluk@gmail.com>
2026-01-17 19:03:27 +0100

    chore(deps): bump up dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,15 +8,15 @@ dependencyResolutionManagement {
     create("libs") {
       plugin("spring.boot", "org.springframework.boot").version("3.5.9")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
-      plugin("jooq", "nu.studer.jooq").version("10.1.1")
+      plugin("jooq", "nu.studer.jooq").version("10.2")
 
       plugin("versions", "com.github.ben-manes.versions").version("0.53.0")
       plugin("spotless", "com.diffplug.spotless").version("8.1.0")
-      plugin("errorprone", "net.ltgt.errorprone").version("4.3.0")
+      plugin("errorprone", "net.ltgt.errorprone").version("4.4.0")
 
       version("springCloudAws", "3.4.2")
-      version("springDoc", "2.8.14")
-      version("awsSdk", "2.40.12")
+      version("springDoc", "2.8.15")
+      version("awsSdk", "2.41.10")
       version("transactionoutbox", "6.1.653") // BE AWARE: check migration scripts before bumping the version up
       version("javaMoney", "1.4.5")
     }
@@ -25,7 +25,7 @@ dependencyResolutionManagement {
     }
     create("toolLibs") {
       version("jspecify", "1.0.0")
-      version("errorprone", "2.45.0")
+      version("errorprone", "2.46.0")
       version("nullaway", "0.12.15")
     }
   }
