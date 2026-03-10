23feb16789d2403e77c149c5dd15b2e3d7e4606f
Łukasz Krauzowicz <kraluk@gmail.com>
2026-03-10 22:51:07 +0100

    chore(deps): bump up spring boot to 4.0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,17 @@ dependencyResolutionManagement {
       version("java", "25")
     }
     create("libs") {
-      plugin("spring.boot", "org.springframework.boot").version("4.0.2")
+      plugin("spring.boot", "org.springframework.boot").version("4.0.3")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
       plugin("jooq", "nu.studer.jooq").version("10.2")
 
       plugin("versions", "com.github.ben-manes.versions").version("0.53.0")
-      plugin("spotless", "com.diffplug.spotless").version("8.2.1")
-      plugin("errorprone", "net.ltgt.errorprone").version("5.0.0")
+      plugin("spotless", "com.diffplug.spotless").version("8.3.0")
+      plugin("errorprone", "net.ltgt.errorprone").version("5.1.0")
 
       version("springCloudAws", "4.0.0")
-      version("springDoc", "3.0.1")
-      version("awsSdk", "2.41.19")
+      version("springDoc", "3.0.2")
+      version("awsSdk", "2.42.9")
       version("transactionoutbox", "6.2.683") // BE AWARE: check migration scripts before bumping the version up
       version("javaMoney", "1.4.5")
     }
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
     }
     create("toolLibs") {
       version("jspecify", "1.0.0")
-      version("errorprone", "2.46.0")
+      version("errorprone", "2.48.0")
       version("nullaway", "0.13.1")
     }
   }
